ef96849258e481ab2bca7de53cb18328273a7a57
Abdullah Nasib U <73380043+nasib96@users.noreply.github.com>
2020-11-05 12:20:52 +0530

    Imported Session2 Custom Package

--- Assets/Scripts/Homework/Session1Homework.cs
+++ Assets/Scripts/Homework/Session1Homework.cs
@@ -2,15 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Session1Homework : MonoBehaviour {
-
+public class Session1Homework : MonoBehaviour 
+{
+	//Numbers
+	public int wholeNumberA = 2;
+	public int wholeNumberB = 4;
+	public float decimalNumberOne = 1.5f;
+	public double lDecimalNumberX = 1.95555555555555555555555555d;
+	//Text
+	public string firstWord = "Welcome";
+	private string secondWord = "This is a private string";
+	//Logical
+	public bool proceed = true;
+	public bool yes = false;
+	//Array
+	public string[] wordSet = new string[3];
+	public int[] numberSet = { 1, 5, 7, 9 };
+	// List
+	public List<int> numberList = new List<int>();
+	// Function
+	private int SumCalculator(int _NumberX, int _NumberY)
+	{
+		int SumResult = _NumberX + _NumberY;
+		return SumResult;
+	}
+	// Dictionary
+	IDictionary<char, string> firstDictionary = new Dictionary<char, string>();
+	// Loop
+	string[] birds = { "Eagle", "Hawk", "Raven", "Sparrow" };
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		Debug.Log(SumCalculator(wholeNumberA, wholeNumberB));
+
+		// Dictionary
+		firstDictionary.Add('A', "Ant Apple Arrow");
+		firstDictionary.Add('B', "Ball Bat Basket");
+		firstDictionary.Add('C', "Cat Column Container");
+		firstDictionary.Add('D', "Dates Diamond Dove");
+		Debug.Log(firstDictionary['C']);
+		firstDictionary['B'] = "Bird Bow Balloon";
+		Debug.Log(firstDictionary['B']);
+
+		for (int Counter = 0; Counter < birds.Length; Counter++)
+		{
+			Debug.Log("Position of" + " " + birds[Counter]);
+			Debug.Log(Counter);
+		}
+
+		int sumOfEvenNumbers = 0;
+		for (int i = 2; i < 200; i = i + 2)
+		{
+			sumOfEvenNumbers += i;
+		}
+		Debug.Log("Total" + " " + sumOfEvenNumbers);
+
+		foreach (string wings in birds)
+		{
+			Debug.Log("Fly" + " " + wings);
+		}
+		//While loop, Always mention the end condition
+		int birdCounter = 0;
+		string birdPark = " birds in the cage";
+		while (birdCounter < birds.Length)
+		{
+			birdPark += " " + birdCounter + birds[birdCounter] + ",";
+			birdCounter += 2;
+		}
+		Debug.Log(birdPark);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 }
